5d8f4716149aee1301cbeb28d3ca4929f0c2f99d
Jafestro <jafestro@gmail.com>
2024-04-10 10:12:05 +0300

    02-express assignment 1

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
+'use strict';
+
 import express from 'express';
-const hostname = '127.0.0.1';
+const hostname = 'localhost';
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Welcome to my REST API!');
+  res.send('Hello world');
+});
+
+app.get('/api/v1/cat', (req, res) => {
+  const cat = {
+    cat_id: 12,
+    name: 'Fluffy',
+    birthdate: '2018-01-01',
+    weight: 5.2,
+    owner: 'Jafar',
+    image: 'localhost:3000/public/cat.png',
+  };
+  res.json(cat);
 });
 
 app.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+app.use('/public', express.static('public'));
